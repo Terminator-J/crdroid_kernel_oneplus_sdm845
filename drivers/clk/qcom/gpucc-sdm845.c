5814954827692e99abf31d7734364489efc9371a
Terminator-J <terminator_j@hotbutteredmonkey.com>
2023-06-03 17:29:52 -0700

    GPU: Drop highest OC freq from 842 to 812MHz
    Everyone was having stability issues at 842MHz, try slightly
    less ambitious (still >100MHz over stock).

--- drivers/clk/qcom/gpucc-sdm845.c
+++ drivers/clk/qcom/gpucc-sdm845.c
@@ -285,7 +285,7 @@ static const struct freq_tbl  ftbl_gpu_cc_gx_gfx3d_clk_src_sdm845_v2[] = {
 	F(636000000, P_CRC_DIV,  1, 0, 0),
 	F(710000000, P_CRC_DIV,  1, 0, 0),
 	F(787000000, P_CRC_DIV,  1, 0, 0),
-	F(842000000, P_CRC_DIV,  1, 0, 0),
+	F(812000000, P_CRC_DIV,  1, 0, 0),
 	{ }
 };
 
@@ -647,7 +647,7 @@ static void gpu_cc_gfx_sdm845_fixup_sdm845v2(void)
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_1] = 710000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_2] = 787000000;
 	gpu_cc_gx_gfx3d_clk_src.clkr.hw.init->rate_max[VDD_GX_HIGH_L1] =
-		842000000;
+		812000000;
 }
 
 static void gpu_cc_gfx_sdm845_fixup_sdm670(void)
