c0f548bc32035267a567f2b7cf19712213df5b7e
Terminator-J <terminator_j@hotbutteredmonkey.com>
2024-06-05 17:56:15 -0700

    Revert "qcacmn: Enable/Disable SRD support for vdev"
    This reverts commit 3bb67651f2d8b030fa6ec6145823bdbfe2375ed2.

--- drivers/staging/qca-wifi-host-cmn/umac/regulatory/dispatcher/inc/wlan_reg_services_api.h
+++ drivers/staging/qca-wifi-host-cmn/umac/regulatory/dispatcher/inc/wlan_reg_services_api.h
@@ -394,17 +394,14 @@ bool wlan_reg_is_etsi13_regdmn(struct wlan_objmgr_pdev *pdev);
 
 /**
  * wlan_reg_is_etsi13_srd_chan_allowed_master_mode() - Checks if regdmn is
- * ETSI13 and SRD channels are allowed in master mode or not for particular
- * vdev.
+ * ETSI13 and SRD channels are allowed in master mode or not.
  *
  * @pdev: pdev ptr
- * @vdev_opmode: vdev opmode
  *
  * Return: true or false
  */
-bool
-wlan_reg_is_etsi13_srd_chan_allowed_master_mode(struct wlan_objmgr_pdev *pdev,
-					        enum QDF_OPMODE vdev_opmode);
+bool wlan_reg_is_etsi13_srd_chan_allowed_master_mode(struct wlan_objmgr_pdev
+						     *pdev);
 
 /**
  * wlan_reg_is_passive_or_disable_ch () - Checks chan state for passive
